2a7ef29eeeaed20f2c3176aa1ecb27957ba558b1
Yannick Eich <ye@sct.de>
2022-11-09 14:42:33 +0100

    simplified Design

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,23 @@ import '../styles/globals.css'
 
 export default function HomePage() {
     return (
-        <div id="container">
-            <h1>Yannick Eich</h1>
-            <h2>To be developed...</h2>
+        <div className="container">
+            <div className="card">
+                {/*<div className="card-image">*/}
+                {/*    <Image src="/images/me.png" alt="Yannick Eich" width={433} height={577}/>*/}
+                {/*</div>*/}
+                <div className="card-loading">
+                    <div className="card-loading-loader">
+                        <h1>Yannick Eich</h1>
+                        <span></span>
+                    </div>
+                </div>
+                <div className="card-text">
+                    <h2>Backend Developer</h2>
+                    <p>JavaScript, PHP, Java</p>
+                    <p style={{marginTop: '20px'}}>Website in Conception...</p>
+                </div>
+            </div>
         </div>
     );
 }
