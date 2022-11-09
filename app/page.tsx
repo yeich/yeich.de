979e77e1aa95a1dc4abe5b49f9dbecec711285c1
Yannick Eich <ye@sct.de>
2022-11-09 14:44:09 +0100

    removed image linted css file

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ export default function HomePage() {
     return (
         <div className="container">
             <div className="card">
-                {/*<div className="card-image">*/}
-                {/*    <Image src="/images/me.png" alt="Yannick Eich" width={433} height={577}/>*/}
-                {/*</div>*/}
                 <div className="card-loading">
                     <div className="card-loading-loader">
                         <h1>Yannick Eich</h1>
